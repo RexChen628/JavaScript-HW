1177175e4dbc3bcb1c6b4ca17a555ff2b340fec1
Rex Chen <rexchen628@hotmail.com>
2022-03-24 22:16:45 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+// 程式碼寫這裡
+
+const ul = document.querySelector(".siteList")
+
+async function getStops(input) {
+  const API =
+    "https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json"
+  const rawData = await fetch(API)
+  const stops = await rawData.json()
+  const matched_result = stops.filter((element) => element.ar.includes(input))
+  matched_result.forEach((element) => {
+    const ul = document.querySelector(".siteList")
+    const output = `<li class="list-group-item fs-5">
+    <i class="fas fa-bicycle"></i>
+    ${element.sna.replace("YouBike2.0_", "")}(${element.tot})<br />
+    <small class="text-muted">${element.ar}</small>
+    </li>`
+    ul.insertAdjacentHTML("afterbegin", output)
+  })
+}
+
+document.querySelector("#searchForm").addEventListener("submit", function (e) {
+  e.preventDefault()
+  const searchKeyword = document.querySelector("#searchKeyword")
+  if (searchKeyword.value !== "") {
+    ul.innerHTML = ""
+    getStops(searchKeyword.value)
+  }
+})
